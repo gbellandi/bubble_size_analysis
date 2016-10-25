94ed61d4f27fee8fd3ef6c897e206aca857ef7dd
stijnvanhoey <stijnvanhoey@gmail.com>
2016-10-25 20:12:18 +0200

    check the functioning of the pipelin function

--- examples/example_bubble.py
+++ examples/example_bubble.py
@@ -1,27 +1,35 @@
 
 import matplotlib.pyplot as plt
 
-from bubblekicker import BubbleKicker
+from bubblekicker.bubblekicker import BubbleKicker
 
 # setup the image handling object
 import os
 print(os.listdir("./"))
 
-my_image = BubbleKicker('Bubble_test_053.bmp', channel='red')
+my_image = BubbleKicker('drafts/0325097m_0305.tif', channel='red')
+
+
+#
+my_image.perform_pipeline_opencv([100, 150], 3, 3, 1, 1)
+
 
 # edge detect
-my_image.edge_detect_image(3, threshold=[None, None])
+#my_image.edge_detect_skimage(3, threshold=[None, None])
+my_image.plot()
+my_image.what_have_i_done()
 
-fig, ax = plt.subplots()
-ax.imshow(my_image.current_image, cmap=plt.cm.gray)
-ax.set_title("edges detected")
 
-# Dilate
-my_image.dilate_image()
-
-fig, ax = plt.subplots()
-ax.imshow(my_image.current_image, cmap=plt.cm.gray)
-ax.set_title("dilated image")
+# fig, ax = plt.subplots()
+# ax.imshow(my_image.current_image, cmap=plt.cm.gray)
+# ax.set_title("edges detected")
+#
+# # Dilate
+# my_image.dilate_skimage()
+#
+# fig, ax = plt.subplots()
+# ax.imshow(my_image.current_image, cmap=plt.cm.gray)
+# ax.set_title("dilated image")
 
 
 #...
