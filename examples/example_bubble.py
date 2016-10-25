65b8a8f932d11975890032ad482e89ea15589038
stijnvanhoey <stijnvanhoey@gmail.com>
2016-10-25 22:31:50 +0200

    setup the custom pipeline class structure in the example

--- examples/example_bubble.py
+++ examples/example_bubble.py
@@ -24,7 +24,7 @@ bubbler.what_have_i_done()
 # setup the object
 bubbler = BubbleKicker('drafts/0325097m_0305.tif', channel='red')
 # using functions (both opencv as skimage are available)
-bubbler.edge_detect_opencv([30, 80])
+bubbler.edge_detect_canny_opencv([30, 80])
 bubbler.dilate_opencv(3)
 # show the resulting image of the detected bubbles
 bubbler.plot()
@@ -35,7 +35,7 @@ bubbler.what_have_i_done()
 bubbler.reset_to_raw()
 
 # some alternative settings
-bubbler.edge_detect_opencv([100, 150])
+bubbler.edge_detect_canny_opencv([100, 150])
 bubbler.dilate_opencv(3)
 bubbler.clear_border_skimage(3, 1)
 # show the resulting image of the detected bubbles
@@ -44,4 +44,22 @@ bubbler.plot()
 # this is the list since the reset to raw
 bubbler.what_have_i_done()
 
+bubbler.reset_to_raw()
+bubbler.adaptive_threshold_opencv()
+bubbler.clear_border_skimage()
+bubbler.plot()
+bubbler.what_have_i_done()
+
+######################
+# EXAMPLE 3
+######################
+
+from bubblekicker.pipelines import CannyPipeline
+
+bubbler = CannyPipeline('drafts/0325097m_0305.tif', channel='red')
+result = bubbler.run([120, 180], 3, 3, 1, 1)
+bubbler.plot()
+
+
 plt.show()
+
