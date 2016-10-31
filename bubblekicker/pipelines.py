2b398d45ab838c20d3e6e8253a54009fe9eb61be
unknown <Giacomo@home243kbr1.UGent.be>
2016-10-31 16:27:18 +0100

    add documentation to functions

--- bubblekicker/pipelines.py
+++ bubblekicker/pipelines.py
@@ -9,8 +9,25 @@ class CannyPipeline(BubbleKicker):
 
     def run(self, threshold, dilate_footprint, border_buffer_size,
             border_bgval, erode_footprint):
-        """execute the different algorithms as a pipeline
-        with given settings"""
+        """Execute the different algorithms as a pipeline
+        with given settings
+        Parameters
+        ----------
+        threshold: [n, m]
+            array of two parameters representing min and max 
+            thresholds for the hysteresis procedure of the opencv
+            Canny method
+        dilate_footprint: int
+            footprint (kernel) of the opencv dilate function.
+            Should be an odd number.
+        border_buffer_size: int
+            width of the border around the image used to clear 
+            possible partial objects
+        border_bgval: int
+            value to be given to the border touching objects
+        
+        """
+
         self.edge_detect_canny_opencv(threshold)
         self.dilate_opencv(dilate_footprint)
         self.fill_holes_opencv()
